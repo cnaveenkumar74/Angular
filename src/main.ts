e72e8d53d4a86ed8feb59422c6fa00b15e0f409b
cnaveenkumar74 <c.naveenkumar74@gmail.com>
2021-10-14 20:48:32 +0530

    Printing date and time

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
+
+//Printing today date and time
+console.log('Today Date and time:');
+console.log(new Date().toLocaleString());
